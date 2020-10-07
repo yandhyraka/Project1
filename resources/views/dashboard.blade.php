27a8f8bad97f76f3bc59f6b49b9d9f6b1dbde8fa
Yandhy Raka <the_raka98@yahoo.co.id>
2020-10-07 16:54:29 +0700

    Css fix 2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     </head>
 
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl yellowed-font leading-tight">
             {{ __('Home') }}
         </h2>
     </x-slot>
